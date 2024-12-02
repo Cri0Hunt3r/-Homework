cdb10766979b3013d1d768e5db7b0b99d6d4fa64
Evgeniy Nikolaev <criohunter2009@gmail.com>
2024-12-02 22:42:50 +0700

    homework commit

---
+++ src/com/nikolaev/Middle2.java
@@ -0,0 +1,13 @@
+package com.nikolaev;
+
+import java.util.Scanner;
+
+public class Middle2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int Diss = (b * b) - (4 * a * c);
+        System.out.println(Diss);
+    }}
---
+++ src/com/nikolaev/Middle3.java
@@ -0,0 +1,9 @@
+package com.nikolaev;
+
+public class Middle3 {
+    public static void main(String[] args) {
+        String Hell = "Hello Java";
+        int value = Hell.indexOf('J');
+        System.out.println(value);
+        System.out.println(Hell.toUpperCase());
+    }}
