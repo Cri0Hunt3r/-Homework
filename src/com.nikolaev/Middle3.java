292787524b32622f7700f3f858408f3e8add2c47
Evgeniy Nikolaev <criohunter2009@gmail.com>
2024-12-02 22:36:15 +0700

    fix Commit

--- src/com.nikolaev/Middle3.java
+++
@@ -1,9 +0,0 @@
-package com.nikolaev;
-
-public class Middle3 {
-    public static void main(String[] args) {
-        String Hell = "Hello Java";
-        int value = Hell.indexOf('J');
-        System.out.println(value);
-        System.out.println(Hell.toUpperCase());
-    }}
